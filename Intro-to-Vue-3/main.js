7339c458ba275bea166db7d79b478a9233bff029
Ioan Velev <ioanvelev46@gmail.com>
2024-11-08 20:30:58 +0200

    Implement list rendering

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -1,10 +1,14 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      inStock: 100,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2345, color: "blue" },
+        { id: 2346, color: "green" },
+      ],
+    };
+  },
+});
