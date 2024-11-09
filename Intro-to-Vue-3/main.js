26e8f6417fd9aa6d9ccd7f2b55c664bb08305de9
Ioan Velev <ioanvelev46@gmail.com>
2024-11-09 16:53:38 +0200

    Update resources

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -1,14 +1,15 @@
 const app = Vue.createApp({
   data() {
-    return {
-      product: "Socks",
-      image: "./assets/images/socks_blue.jpg",
-      inStock: 100,
-      details: ["50% cotton", "30% wool", "20% polyester"],
-      variants: [
-        { id: 2345, color: "blue" },
-        { id: 2346, color: "green" },
-      ],
-    };
-  },
+      return {
+          cart:0,
+          product: 'Socks',
+          image: './assets/images/socks_blue.jpg',
+          inStock: true,
+          details: ['50% cotton', '30% wool', '20% polyester'],
+          variants: [
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+          ]
+      }
+  }
 });
