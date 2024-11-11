024ff6b0fb27c0dc87beef378dccb29295bd615a
Ioan Velev <ioanvelev46@gmail.com>
2024-11-11 22:53:09 +0200

    Pass prop to component

--- Intro-to-Vue-3/components/ProductDisplay.js
+++ Intro-to-Vue-3/components/ProductDisplay.js
@@ -1,4 +1,10 @@
 app.component('product-display', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
     template: 
     /*html*/
     `<div class="product-display">
@@ -10,6 +16,8 @@ app.component('product-display', {
             <h1>{{ title }}</h1>
             <p v-if="inStock">In Stock</p>
             <p v-else>Out of Stock</p>
+
+            <p>Shipping: {{ shipping }}</p>
             <ul>
               <li v-for="detail in details">{{ detail }}</li>
             </ul>
@@ -54,6 +62,9 @@ app.component('product-display', {
       },
       inStock() {
           return this.variants[this.selectedVariant].quantity;
-      }
+      },
+       shipping() {
+        return this.premium ? 'Free' : 2.99;
+       }
     }
 });
