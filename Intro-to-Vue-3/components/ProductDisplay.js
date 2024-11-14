f4c30bd286315f051c82cc82fb5eee83b79e4b53
Ioan Velev <ioanvelev46@gmail.com>
2024-11-14 17:38:32 +0200

    Add listener on form submit

--- Intro-to-Vue-3/components/ProductDisplay.js
+++ Intro-to-Vue-3/components/ProductDisplay.js
@@ -32,6 +32,7 @@ app.component('product-display', {
             <button class="button" :class="{ disabledButton: !inStock }" :disabled="!inStock" @click="addToCart">Add to Cart</button>
           </div>
         </div>
+        <review-form @review-submitted="addReview"></review-form>
       </div>`,
       data() {
         return {
@@ -42,7 +43,8 @@ app.component('product-display', {
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ]
+            ],
+            reviews: []
         }
     },
     methods: {
@@ -51,6 +53,9 @@ app.component('product-display', {
         },
         updateVariant(index) {
             this.selectedVariant = index;
+        },
+        addReview(review) {
+          this.reviews.push(review);
         }
     },
     computed: {
